cbb4420e43936abcd18afeca819be7fe3f29350d
ivanlipotan <15017117@myrp.edu.sg>
2017-05-18 17:20:55 +0800

    seconfd

--- app/src/main/java/sg/edu/rp/c346/p05_ndpsongs/SecondActivity.java
+++ app/src/main/java/sg/edu/rp/c346/p05_ndpsongs/SecondActivity.java
@@ -1,8 +1,10 @@
 package sg.edu.rp.c346.p05_ndpsongs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -22,24 +24,35 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         lv = (ListView)findViewById(R.id.lv);
-        btn5Star = (Button)findViewById(R.id.btn5star);
+        btn5Star = (Button)findViewById(R.id.btn5Star);
+
+
         al = new ArrayList<Song>();
 
         DBHelper dbh = new DBHelper(this);
-        ArrayList<Song>songs = dbh.getAllNotes();
-        adapter = new RevisionNotesArrayAdapter(this, R.layout.row, notes);
-        lv.setAdapter(adapter);
+        ArrayList<Song>songs = dbh.getAllSongs();
+        aa = new songAdapter(this, R.layout.row, songs);
+        lv.setAdapter(aa);
+
 
 
         btn5Star.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DBHelper dbh = new DBHelper(MainActivity.this);
-
-                String
+                DBHelper dbh = new DBHelper(SecondActivity.this);
                 al.clear();
-                al.add(dbh.getAllNotes());
+                al.addAll(dbh.get5StarSongs());
+                dbh.close();
+
+                for (int i = 0; i < al.size(); i++) {
+                    String tmp = al.get(i).toString();
+                }
+                lv.setAdapter();
+                aa.notifyDataSetChanged();
             }
         });
+
     }
+
+
 }
