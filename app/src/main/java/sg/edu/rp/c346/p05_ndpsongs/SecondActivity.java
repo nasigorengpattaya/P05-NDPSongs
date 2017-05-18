55f3a8efe60e5ac6829c70405b12f27a19428f1d
ivanlipotan <15017117@myrp.edu.sg>
2017-05-18 15:10:27 +0800

    try

--- app/src/main/java/sg/edu/rp/c346/p05_ndpsongs/SecondActivity.java
+++ app/src/main/java/sg/edu/rp/c346/p05_ndpsongs/SecondActivity.java
@@ -3,14 +3,11 @@ package sg.edu.rp.c346.p05_ndpsongs;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
+        setContentView(R.layout.activity_second);
     }
 }
